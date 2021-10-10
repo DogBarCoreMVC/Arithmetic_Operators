b1dfeba5f594aa9bfd67c0fe8a25ea79b2024f3f
GitDogBar <87788268+DogBarCoreMVC@users.noreply.github.com>
2021-10-10 15:15:50 +0700

    add index.html and index.js
    บวก ลบ คูณ หาร หารเอาเศษ ยกกำลัง

---
+++ Index.js
@@ -0,0 +1,20 @@
+let Result_Add = 15 + 32; // Addition 47
+console.log(Result_Add);
+
+let Result_Sub = 12 - 7; // Subtraction 5
+console.log(Result_Sub);
+
+let Result_Mul = 6 * 5; // Multilplication 30
+console.log(Result_Mul);
+
+let Result_Div = 9 / 3; // Divistion 3
+console.log(Result_Div);
+
+let Result_Modu = 7 % 3; // Modulus หารเอาเศษมาตอบ 1
+console.log(Result_Modu);
+
+let Result_Expon = 6 ** 3; // Exponentiation 216
+console.log(Result_Expon);
+
+let Result = (8 + 5) * (9 - 4); // บังคับให้ทำในวงเล็บก่อน
+console.log(Result);
